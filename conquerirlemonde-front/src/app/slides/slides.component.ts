f869587c91295e6dac29b4b2269a74a4ff3686e4
Antoine Stalin <astalin@norsys.fr>
2017-01-24 17:40:28 +0100

    feat(slides):use single reveal slide to be displayed

--- conquerirlemonde-front/src/app/slides/slides.component.ts
+++ conquerirlemonde-front/src/app/slides/slides.component.ts
@@ -10,12 +10,18 @@ import { ActivatedRoute, Params }   from '@angular/router';
 export class SlidesComponent implements OnInit {
 
   id: string;
+  matcher = {
+    "intro" : "#/",
+    "episode1" : "#/1",
+    "episode2" : "#/2",
+    "episode3" : "#/3"
+  }
 
   constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
-      this.id = params['id'];
+      this.id = this.matcher[params['id']];
     });
     document.getElementsByTagName("iframe")[0].className = "fullScreen";
   }
