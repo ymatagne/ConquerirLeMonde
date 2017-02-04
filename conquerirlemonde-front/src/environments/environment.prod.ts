e223183b5f7bfd06b9a1d77e013fb350aa80da86
Yan MATAGNE <ymatagne@norsys.fr>
2017-02-04 23:00:32 +0100

    fix(vagrant) : push Vagrant file

--- conquerirlemonde-front/src/environments/environment.prod.ts
+++ conquerirlemonde-front/src/environments/environment.prod.ts
@@ -1,7 +1,13 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
 export const environment = {
-  production: true,
-  planetsUrls: 'ws://localhost:8080/ws/planets',
-  newTrooperUrl: 'ws://localhost:8080/ws/trooper',
-  dropTrooperUrl: 'http://localhost:8080/trooper/die',
-  loginUrl: 'http://172.20.20.57:8082/api/authenticate'
+  production: false,
+  wsPlanetsKubernetes: 'ws://54.154.192.138:8080/ws/planets/kubernetes',
+  wsPlanetsFleet: 'ws://54.154.192.138:8080/ws/planets/fleet',
+  newTrooperUrl: 'ws://54.154.192.138:8080/ws/trooper',
+  dropTrooperUrl: 'http://54.154.192.138:8080/trooper/die',
+  loginUrl: 'http://54.154.192.138:8080/api/authenticate'
 };
